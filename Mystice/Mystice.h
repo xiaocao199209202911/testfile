759b592883f33692d531253fced9d9024a144f01
xiangcang <1150238638@qq.com>
2016-08-29 15:32:17 +0800

    Initial Commit

---
+++ Mystice/Mystice.h
@@ -0,0 +1,13 @@
+//
+//  Mystice.h
+//  Mystice
+//
+//  Created by iOS香肠 on 16/8/29.
+//  Copyright © 2016年 xiangcang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mystice : NSObject
+
+@end
---
+++ Mystice/Mystice.m
@@ -0,0 +1,13 @@
+//
+//  Mystice.m
+//  Mystice
+//
+//  Created by iOS香肠 on 16/8/29.
+//  Copyright © 2016年 xiangcang. All rights reserved.
+//
+
+#import "Mystice.h"
+
+@implementation Mystice
+
+@end
